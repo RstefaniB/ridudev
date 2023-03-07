bb0b821771e5ccd403a1977ce0c0ba33c2c84cd7
RstefaniB <rickies72@gmail.com>
2023-03-07 15:27:12 +0100

    Primer Twitter Card Static

--- projects/01-twitter-follow-card/src/App.jsx
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -1,9 +1,19 @@
 export function App() {
     return (
 
-        <div>
-            <h1>Twitter Card</h1>
-        </div>
+        <article>
+            <h1>A quien seguir...</h1>
+            <header>
+                <img src="https://unavatar.io/rebecaferguson"></img>
+                <div>
+                    <strong>Rebeca Ferguson</strong>
+                    <span>@rebecaferguson</span>
+                </div>
+            </header>
+            <aside>
+                <button>me button</button>
+            </aside>
+        </article>
     )
 }
 
