c61929aa5e41e98c86589b7347311f22bb65cce8
RstefaniB <rickies72@gmail.com>
2023-03-13 21:20:42 +0100

    hemos creado una constante inputRef con un estado Ref. Ademas, una funcion handleClick(revisar <button> element en return de App()) que contiene una constante inputRef(inputRef.current) y otra value(inputRef.current.value). Echar ojo a <input> element abajo en return.

--- projects/05-buscador-movies/src/App.jsx
+++ projects/05-buscador-movies/src/App.jsx
@@ -1,10 +1,18 @@
 import { useMovies } from './hooks/useMovies'
 import './App.css'
 import MappedMovies from './components/Movies'
+import { useEffect, useRef, useState } from 'react'
 
 function App() {
 
   const { movies: mappedMovies } = useMovies()
+  const inputRef = useRef()
+
+  const handleClick = () => {
+    const inputEl = inputRef.current
+    const value = inputEl.value
+    alert(value)
+  }
 
   return (
     <div className='page'>
@@ -12,8 +20,8 @@ function App() {
       <header>
         <h1>Buscador de peliculas</h1>
         <form className='form'>
-          <input type='text' placeholder='Star Wars, Benjamin Button' />
-          <button type='submit'>Search</button>
+          <input ref={inputRef} type='text' placeholder='Star Wars, Benjamin Button' />
+          <button onClick={handleClick} type='submit'>Search</button>
         </form>
       </header >
 
